545152c2ae8d66486460a5203a3af2546512c5f4
nicosiy <nicosiysoo@gmail.com>
2015-09-17 22:57:06 -0400

    upload

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Assignment 2
 
-[Open Project in Editor](http://pucd2035-e-f15.github.io/class_notes/p5_lab.html?https://raw.githubusercontent.com/PUCD2035-E-F15/assignment_2/master/sketch.js)
+[Open Project in Editor](http://pucd2035-e-f15.github.io/class_notes/p5_lab.html?https://raw.githubusercontent.com/nicosiy/assignment_2/master/sketch.js)
 
-> Update the link above by replacing second `PUCD2035-E-F15` with your github name. Delete these instructions when you post.
-
-![Output](output.png)
-
-^ replace output.png with a screen-grab of your program's drawing. Add a second image to show off the variation your program produces.
+![Output](screengrab1.png)
+![Output](screengrab2.png)
--- sketch.js
+++ sketch.js
@@ -1,18 +1,127 @@
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(640, 480);
 	noStroke();
-	noLoop();
+
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 50);
 
-	// set a fill color
-	fill(255, 255, 255);
+	var summerWinter = random(1);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	// draw background and snowman
+	if (summerWinter < 0.5) {
+		// Summer season
+		background(30, 144, 255);
+		fill(50, 205, 50);
+		ellipse(320, 540, 1150, 280);
+
+		// draw sad snowman
+		stroke(0);
+
+		fill(255, 250, 250);
+		ellipse(250, 400, 100, 100)
+		ellipse(250, 330, 85, 85)
+		ellipse(250, 270, 70, 70)
+		fill(0);
+		ellipse(235, 260, 8, 8);
+		ellipse(265, 260, 8, 8);
+		line(230, 280, 270, 280);
+		line(230, 280, 230, 288);
+		line(270, 280, 270, 288);
+	} else {
+		// Winter Season
+		background(175, 238, 238);
+		fill(224, 255, 255);
+		ellipse(320, 540, 1150, 280);
+
+		//draw happy snowman
+
+		stroke(0);
+
+		fill(255, 250, 250);
+		ellipse(250, 400, 100, 100)
+		ellipse(250, 330, 85, 85)
+		ellipse(250, 270, 70, 70)
+		fill(0);
+		ellipse(235, 250, 8, 8);
+		ellipse(265, 250, 8, 8);
+		line(230, 280, 270, 280);
+		line(230, 280, 230, 270);
+		line(270, 280, 270, 270);
+	}
+
+
+
+	// Sun
+	if (summerWinter < 0.5) {
+		fill(255, 255, 0);
+		ellipse(random(320, 640), random(0, 280), 100, 100);
+	}
+
+	// snow!!!!
+	if (summerWinter > 0.5) {
+		fill(250, 250, 250);
+		stroke(0, 0, 128);
+		var snowflakes;
+		snowflakes = random(4, 8);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(4, 8);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(4, 8);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(4, 8);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+		snowflakes = random(2, 5);
+		ellipse(random(0, 640), random(0, 280), snowflakes, snowflakes);
+
+	}
+
+	noLoop();
+
 }
